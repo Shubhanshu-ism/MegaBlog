f0870f7fdce462edf16158f307efbd0d8ca38b55
skj <shubhanshujain2233@gmail.com>
2025-05-19 04:47:47 +0530

    redux tool kit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+
+import { useDispatch } from 'react-redux'
+import './App.css'
+import { useEffect, useState } from 'react'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice '
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then( (userData)=> {
+      if(userData){
+        dispatch(login(userData))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    
+    .finally(()=> setLoading(false))
+  },[])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      
+      <div className='w-full block'>
+          <Header/>
+          <main>
+            {/* <Outlet/> */}
+            todo
+          </main>
+          <Footer/>
+      </div>
+    </div>
+) : 
+(
+  <div>
+    Loading
+  </div>
+)
+}
+
+export default App
