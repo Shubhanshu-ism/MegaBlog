9be2f02253c95c1ea74700b5d6931f09293a256a
skj <shubhanshujain2233@gmail.com>
2025-05-20 03:20:03 +0530

    login and signupb pages

--- src/components/index.js
+++ src/components/index.js
@@ -4,8 +4,21 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from './Login'
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
 
 
-
-
-export { Header, Footer, Container, Logo, LogoutBtn , RTE};
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+};
--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -8,7 +8,7 @@ import { useSelector } from 'react-redux'
 
 
 export default function PostForm({post}) {
-  const {register, handleSubmit, watch, setValue, control, getValuse} =useForm({
+  const {register, handleSubmit, watch, setValue, control, getValues} =useForm({
     defaultValues:{
       title: post?.title || '',
       slug: post?.slug || '',
@@ -21,8 +21,7 @@ export default function PostForm({post}) {
   const submit  = async(data)=>{
     if (post) {
       const file = data.image[0]
-        ? appwriteService.uploadFile(data.image[0])
-        : null;
+        ? appwriteService.uploadFile(data.image[0]) : null;
 
       if (file) {
         appwriteService.deleteFile(post.featuredImage);
@@ -31,6 +30,7 @@ export default function PostForm({post}) {
         ...data,
         featuredImage: file ? file.$id : undefined,
       });
+      
       if (dbPost) {
         navigate(`/post/${dbPost.$id}`);
       }
